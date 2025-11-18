708cf5953a8f0a9acde4892a331fdec24fdf9486
dashboard <c3p31079@bunkyo.ac.jp>
2025-11-18 23:27:19 +0900

    ＠/を追加

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, send_file
-from openpyxl import load_workbook
 from flask_cors import CORS
+from openpyxl import load_workbook
 import os
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/")
+def home():
+    return "Backend running"
 
 @app.route("/generate", methods=["POST"])
 def generate():
@@ -18,13 +22,11 @@ def generate():
 
     wb = load_workbook(template_path)
     ws = wb.active
-
-    ws["B2"] = inspector   # Excel の必要なセルに書き込む
-
+    ws["B2"] = inspector
     wb.save(output_path)
+
     return send_file(output_path, as_attachment=True)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
