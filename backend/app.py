879c234dca703f9e1a1fbd7d550fc32ff5046d32
c3p31079 <146411811+c3p31079@users.noreply.github.com>
2025-12-02 23:19:47 +0900

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,30 @@
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, request, send_file
 from flask_cors import CORS
 from excel_processor import process_excel
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/generate", methods=["POST"])
 def generate():
     data = request.json
 
-    part  = data.get("part")
-    item  = data.get("item")
+    part = data.get("part")
+    item = data.get("item")
     score = float(data.get("score", 0))
+    is_checked = bool(data.get("checked", False))
 
     output_path = "generated.xlsx"
-    process_excel(part, item, score, output_name=output_path)
+    process_excel(part, item, score, is_checked, output_name=output_path)
 
     return send_file(output_path, as_attachment=True)
 
 
+
 @app.route("/", methods=["GET"])
 def home():
-    return "API is working", 200
+    return "API Working", 200
 
 
 if __name__ == "__main__":
